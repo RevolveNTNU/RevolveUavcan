cc5fadc2ed4d7788b49d1012520138d378e63e91
Jostein Tysse <43188104+TechedLemur@users.noreply.github.com>
2021-06-25 09:50:57 +0200

    Add missing method and property to interface (#18)

--- RevolveUavcan/Uavcan/Interfaces/IUavcanSerializationGenerator.cs
+++ RevolveUavcan/Uavcan/Interfaces/IUavcanSerializationGenerator.cs
@@ -1,15 +1,17 @@
 ﻿using RevolveUavcan.Dsdl.Fields;
+using RevolveUavcan.Dsdl.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace RevolveUavcan.Uavcan.Interfaces
 {
     public interface IUavcanSerializationGenerator
     {
+        IDsdlParser DsdlParser { get; }
         Dictionary<Tuple<uint, string>, List<UavcanChannel>> MessageSerializationRules { get; }
         Dictionary<Tuple<uint, string>, UavcanService> ServiceSerializationRules { get; }
 
+        bool Init();
         bool TryGetSerializationRuleForMessage(uint subjectId, out List<UavcanChannel> uavcanChannels);
         bool TryGetSerializationRuleForMessage(string messageName, out List<UavcanChannel> uavcanChannels);
         bool TryGetSerializationRuleForService(uint subjectId, out UavcanService service);
